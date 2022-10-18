aafdb8fc7da32711cb2601a2625cb6dbb3699917
HYEMIN PARK <115875669+aimaimee@users.noreply.github.com>
2022-10-18 19:43:57 +0900

    Update mpg.py

--- pages/mpg.py
+++ pages/mpg.py
@@ -16,7 +16,6 @@ st.markdown("# 자동차 연비 🚗")
 st.sidebar.markdown("# 자동차 연비 🚗")
 
 url = "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/mpg.csv"
-mpg = pd.read_csv(url)
 
 # uber.py를 참고하여 캐시에서 데이터를 불러 올 수 있도록 설정
 @st.cache
@@ -31,34 +30,51 @@ data_load_state.text("Done! (using st.cache)")
 # Sidebar - year
 st.sidebar.header('User Input Features')
 selected_year = st.sidebar.selectbox('Year',
-   list(reversed(range(mpg.model_year.min(),mpg.model_year.max())))
+   list(reversed(range(data.model_year.min(), data.model_year.max())))
    )
 
 if selected_year > 0 :
-   mpg = mpg[mpg.model_year == selected_year]
+   data = data[data.model_year == selected_year]
 
 # Sidebar - origin
-sorted_unique_origin = sorted(mpg.origin.unique())
+sorted_unique_origin = sorted(data.origin.unique())
 selected_origin = st.sidebar.multiselect('origin', sorted_unique_origin, sorted_unique_origin)
 
 if len(selected_origin) > 0:
-   mpg = mpg[mpg.origin.isin(selected_origin)]
+   data = data[data.origin.isin(selected_origin)]
 
 
-st.dataframe(mpg)
+st.dataframe(data)
 
 # Streamlit에서 제공하는 Chart
-st.line_chart(mpg["mpg"])
+st.line_chart(data["mpg"])
 
-st.bar_chart(mpg["mpg"])
+st.bar_chart(data["mpg"])
 
-fig, ax = plt.subplots(figsize=(10,3))
-sns.countplot(data=mpg, x="origin").set_title("지역별 자동차 연비 데이터 수")
-st.pyplot(fig)
+pxh = px.histogram(data, x="origin", title="지역별 자동차 연비 데이터 수")
+st.plotly_chart(pxh)
 
-pxh = px.histogram(mpg, x="origin", title="지역별 자동차 연비 데이터 수")
+pxh = px.histogram(data, x="origin", y="horsepower", histfunc="avg", title="지역별 자동차 마력 평균")
 st.plotly_chart(pxh)
 
 fig, ax = plt.subplots(figsize=(10,3))
-sns.scatterplot(data=mpg, x="mpg", y="weight", hue="origin")
+sns.countplot(data=data, x="origin").set_title("지역별 자동차 연비 데이터 수")
+st.pyplot(fig)
+
+# data에 덮어씌운 데이터셋이 아니라 전체 데이터셋을 가지고 그리는게 더 적합
+fig = sns.lmplot(data=data, x="weight", y="horsepower", hue="origin", ci=None)
+plt.title("지역별 무게와 마력")
+st.pyplot(fig)
+
+# 마찬가지로 전체 데이터셋을 가지고 그리는게 더 적합
+fig, ax = plt.subplots()
+sns.scatterplot(data=data, x="mpg", y="weight", hue='origin').set_title("mpg 별 origin 별 weight 시각화")
+st.pyplot(fig)
+
+fig, ax = plt.subplots()
+sns.scatterplot(data=data, x="horsepower", y="mpg")
+st.pyplot(fig)
+
+fig, ax = plt.subplots()
+sns.violinplot(data=data, x="cylinders", y="mpg")
 st.pyplot(fig)
